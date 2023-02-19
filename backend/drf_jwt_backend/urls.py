795b87cc43c1aee0eb0b3d751f2022ffec09fd3f
Karim <aarrale12@gmail.com>
2023-02-19 15:35:21 -0600

    api

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
-    path('api/todolist', include(router.urls))
+    path('api/', include(router.urls))
 ]
