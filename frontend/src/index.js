e2df173dfa5d2447efd51b1026616f8edc298e92
Karim <aarrale12@gmail.com>
2023-02-19 23:50:12 -0600

    todolist complete and working

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// importing css stylesheet to use the bootstrap class
+import 'bootstrap/dist/css/bootstrap.min.css'; //add this line only in this file
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
